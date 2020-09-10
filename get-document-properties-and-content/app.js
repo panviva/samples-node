04ec3998f740eb0863a6ec827fcf9cd5caf20913
cshehan <cshehan@panviva.com>
2020-09-10 20:35:18 +1000

    Add all sample code

---
+++ get-document-properties-and-content/app.js
@@ -0,0 +1,14 @@
+const { PanvivaClient, ResourceApiKeys} = require('@panviva/node-sdk');
+const {endpoint, instance, key , id} = require('./config');
+
+const panvivaClient = new PanvivaClient(instance, endpoint);
+panvivaClient.setApiKey(ResourceApiKeys.apiKeyHeader, key);
+
+const mergeDoc = async () => {
+  const doc = await panvivaClient.getDocumentById(id)
+  const containers = await panvivaClient.getDocumentByIdContainers(id)
+  return {...doc.body, ...containers.body}
+}
+
+mergeDoc()
+  .then(console.log)
