3a0e37e4346aa54eb25c9ee06f7539a9bf65b79c
cshehan <cshehan@panviva.com>
2020-09-10 15:37:47 +1000

    Add configurable id

--- get-document-properties-and-content-as-plaintext/app.js
+++ get-document-properties-and-content-as-plaintext/app.js
@@ -1,11 +1,11 @@
 const { PanvivaClient, ResourceApiKeys} = require('@panviva/node-sdk');
-const {endpoint, instance, Key} = require('./config');
+const {endpoint, instance, Key , id} = require('./config');
 
 const panvivaClient = new PanvivaClient(instance, endpoint);
 
 panvivaClient.setApiKey(ResourceApiKeys.apiKeyHeader, Key);
 
-panvivaClient.getDocumentById(785).then((response) => {
+panvivaClient.getDocumentById(id).then((response) => {
   
   for (key in response.body){
     console.log(key,response.body[key]);
--- get-document-properties-and-content-as-plaintext/config.js
+++ get-document-properties-and-content-as-plaintext/config.js
@@ -3,5 +3,6 @@ dotenv.config();
 module.exports = {
   endpoint: process.env.API_URL,
   instance: process.env.INSTANCE_NAME,
-  Key: process.env.API_KEY
+  Key: process.env.API_KEY,
+  id: process.env.DOCUMENT_ID
 };
